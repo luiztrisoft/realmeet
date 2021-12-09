1f7e3da3a20473c6bed6be57aa4dfac6a0ed5530
Luiz A. R. da Silva <luiz.ricardo@groupsoftware.com.br>
2021-12-09 20:48:15 -0300

    Initial commit

---
+++ service/src/main/java/br/com/sw2you/realmeet/RoomController.java
@@ -0,0 +1,12 @@
+package br.com.sw2you.realmeet;
+
+//import br.com.sw2you.realmeet.api.facade.RoomsApi;
+import org.springframework.http.ResponseEntity;
+
+import java.util.concurrent.CompletableFuture;
+
+public class RoomController {//implements RoomsApi {
+
+//    public CompletableFuture<ResponseEntity<Room> listRooms(){    }
+
+}
