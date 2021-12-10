ea28be73d836fcf59747ef33260308b1080fcb74
Luiz A R Silva <luiz.trisoft@gmail.com>
2021-12-09 22:16:31 -0300

    Config inicial para pipeline

--- service/src/main/java/br/com/sw2you/realmeet/Application.java
+++ service/src/main/java/br/com/sw2you/realmeet/Application.java
@@ -1,7 +1,7 @@
 package br.com.sw2you.realmeet;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 public class Application {
--- service/src/main/java/br/com/sw2you/realmeet/RoomController.java
+++ service/src/main/java/br/com/sw2you/realmeet/RoomController.java
@@ -1,12 +1,12 @@
 package br.com.sw2you.realmeet;
 
 //import br.com.sw2you.realmeet.api.facade.RoomsApi;
-import org.springframework.http.ResponseEntity;
-
+//import br.com.sw2you.realmeet.api.model.RoomDTO;
 import java.util.concurrent.CompletableFuture;
+import org.springframework.http.ResponseEntity;
 
 public class RoomController {//implements RoomsApi {
 
-//    public CompletableFuture<ResponseEntity<Room> listRooms(){    }
+    //public CompletableFuture<ResponseEntity<RoomDTO> listRooms(){    }
 
 }
